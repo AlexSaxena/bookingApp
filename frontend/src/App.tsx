f5d1795798add4708a8065f5aac7f2a90c6e6596
Alex Saxena <saxenaalex@gmail.com>
2025-10-01 00:11:44 +0200

    Adds basic step Wizard setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
+type Step = "landing" | "booking" | "personalInfo" | "confirmation";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-4xl text-blue-600">Vite + React + TS + Tailwind</h1>
-    </>
-  );
+  const [step, setStep] = useState<Step>("landing");
+
+  if (step === "landing") {
+    return (
+      <>
+        <p>Landing</p>
+        <button onClick={() => setStep("booking")}>Continue</button>
+      </>
+    );
+  }
+  if (step === "booking") {
+    return (
+      <>
+        <p>Booking</p>
+        <button onClick={() => setStep("personalInfo")}>Continue</button>
+      </>
+    );
+  }
+  if (step === "personalInfo") {
+    return (
+      <>
+        <p>Personal Info</p>{" "}
+        <button onClick={() => setStep("confirmation")}>Continue</button>
+      </>
+    );
+  }
+  if (step === "confirmation") {
+    return <p>Confirmation</p>;
+  }
+
+  return <p>How dig you get here?</p>;
 }
 
 export default App;
