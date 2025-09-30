8ed69b432af2687e6472535709f35a0cfc550f52
Alex Saxena <saxenaalex@gmail.com>
2025-09-30 23:25:34 +0200

    Removes incorrect tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-blue-600">Vite + React</h1>
+      <h1 className="text-4xl text-blue-600">Vite + React + TS + Tailwind</h1>
     </>
   );
 }
