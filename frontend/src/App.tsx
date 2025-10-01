fc8cc936ad04206b9a507c939be6e6736b12a2bd
Alex Saxena <saxenaalex@gmail.com>
2025-10-01 12:02:49 +0200

    Adds state for different steps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,12 @@ type Step = "landing" | "booking" | "personalInfo" | "confirmation";
 
 function App() {
   const [step, setStep] = useState<Step>("landing");
+  const [name, setName] = useState<string>("");
+  const [room, setRoom] = useState<{
+    roomId: number;
+    date: string;
+    hour: number;
+  } | null>(null);
 
   if (step === "landing") {
     return (
@@ -24,7 +30,25 @@ function App() {
     return (
       <div className="min-h-screen grid place-items-center bg-gray-50">
         <div className="max-w-sm w-full p-6 text-center">
-          <h1 className="text-4xl font-bold mb-8 text-left">Booking</h1>
+          <h1 className="text-4xl font-bold mb-8 text-left">Välj en tid</h1>
+          <div>
+            <button
+              onClick={() =>
+                setRoom({ roomId: 1, date: "2024-07-01", hour: 9 })
+              }
+              className="rounded py-2 border-amber-600 border-2 px-4 mr-4 mb-4 hover:bg-amber-600 hover:text-white cursor-pointer"
+            >
+              Tid 1 Steve
+            </button>
+            <button
+              onClick={() =>
+                setRoom({ roomId: 2, date: "2024-07-01", hour: 10 })
+              }
+              className="rounded py-2 border-amber-600 border-2 px-4 mr-4 mb-4 hover:bg-amber-600 hover:text-white cursor-pointer"
+            >
+              Tid 2 Ada
+            </button>
+          </div>
           <button
             onClick={() => setStep("personalInfo")}
             className="w-full px-6 py-3 rounded bg-black text-white"
@@ -39,12 +63,20 @@ function App() {
     return (
       <div className="min-h-screen grid place-items-center bg-gray-50">
         <div className="max-w-sm w-full p-6 text-center">
-          <h1 className="text-4xl font-bold mb-8 text-left">Personal Info</h1>
+          <h1 className="text-4xl font-bold mb-8 text-left">Vem bokar?</h1>
+          <p className="font-bold text-left mb-2">Förnamn och efternamn</p>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Skriv ditt fullständiga namn här"
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
           <button
             onClick={() => setStep("confirmation")}
             className="w-full px-6 py-3 rounded bg-black text-white"
           >
-            Nästa
+            Boka
           </button>
         </div>
       </div>
@@ -54,11 +86,18 @@ function App() {
     return (
       <div className="min-h-screen grid place-items-center bg-gray-50">
         <div className="max-w-sm w-full p-6 text-center">
-          <h1 className="text-3xl font-bold mb-8 text-left">
+          <h1 className="text-2xl font-bold mb-8 text-left">
             Ditt rum är nu bokat!
           </h1>
+          <p>
+            Du är {name} och valde rum {room?.roomId} {room?.date}
+            {room?.hour}
+          </p>
           <button
-            onClick={() => setStep("landing")}
+            onClick={() => {
+              setStep("landing");
+              setName("");
+            }}
             className="w-full px-6 py-3 rounded bg-black text-white"
           >
             Tillbaka
