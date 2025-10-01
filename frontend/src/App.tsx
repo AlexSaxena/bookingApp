73d5c20a130ec735fb3bcda1d3de9d6560fbde71
Alex Saxena <saxenaalex@gmail.com>
2025-10-01 11:12:43 +0200

    Adds basic styling for steps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,22 +22,50 @@ function App() {
   }
   if (step === "booking") {
     return (
-      <>
-        <p>Booking</p>
-        <button onClick={() => setStep("personalInfo")}>Continue</button>
-      </>
+      <div className="min-h-screen grid place-items-center bg-gray-50">
+        <div className="max-w-sm w-full p-6 text-center">
+          <h1 className="text-4xl font-bold mb-8 text-left">Booking</h1>
+          <button
+            onClick={() => setStep("personalInfo")}
+            className="w-full px-6 py-3 rounded bg-black text-white"
+          >
+            Nästa
+          </button>
+        </div>
+      </div>
     );
   }
   if (step === "personalInfo") {
     return (
-      <>
-        <p>Personal Info</p>{" "}
-        <button onClick={() => setStep("confirmation")}>Continue</button>
-      </>
+      <div className="min-h-screen grid place-items-center bg-gray-50">
+        <div className="max-w-sm w-full p-6 text-center">
+          <h1 className="text-4xl font-bold mb-8 text-left">Personal Info</h1>
+          <button
+            onClick={() => setStep("confirmation")}
+            className="w-full px-6 py-3 rounded bg-black text-white"
+          >
+            Nästa
+          </button>
+        </div>
+      </div>
     );
   }
   if (step === "confirmation") {
-    return <p>Confirmation</p>;
+    return (
+      <div className="min-h-screen grid place-items-center bg-gray-50">
+        <div className="max-w-sm w-full p-6 text-center">
+          <h1 className="text-3xl font-bold mb-8 text-left">
+            Ditt rum är nu bokat!
+          </h1>
+          <button
+            onClick={() => setStep("landing")}
+            className="w-full px-6 py-3 rounded bg-black text-white"
+          >
+            Tillbaka
+          </button>
+        </div>
+      </div>
+    );
   }
 
   return <p>How dig you get here?</p>;
